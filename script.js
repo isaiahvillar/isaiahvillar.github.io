7edf1958944f47c880cfd31d4ec798a814903645
isaiahvillar <68302141+isaiahvillar@users.noreply.github.com>
2023-08-03 14:47:48 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+const images = document.querySelectorAll(".images img");
+const modal = document.querySelector(".modal");
+const modalImg = document.querySelector(".modalImg");
+const modalTxt = document.querySelector(".modalTxt");
+const close = document.querySelector(".close");
+
+images.forEach((image) => {
+  image.addEventListener("click", () => {
+    modalImg.src = image.src;
+    modalTxt.innerHTML = image.alt;
+    modal.classList.add("appear");
+
+    close.addEventListener("click", () => {
+      modal.classList.remove("appear");
+    });
+  });
+});
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
